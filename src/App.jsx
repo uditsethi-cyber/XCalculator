0f3f91e67a6e0bfc5f3b3192ea6a6f9edea4e31c
Udit Sethi <udit.s18@iiits.in>
2026-09-16 18:02:44 +0530

    Calculator implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,85 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Grid } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [result, setResult] = useState("");
+  const [exp, setExp] = useState("");
+  const showResult = () => {
+    try {
+      const res = eval(exp);
+      setResult(res);
+    } catch (e) {
+      setResult("Error");
+    }
+  };
+  const handleExp = (e) => {
+    console.log(e);
+    setExp((prev) => prev + e.target.innerText);
+  };
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+        <h3>React Calculator</h3>
+        <input
+          value={exp}
+          onChange={(e) => setExp(e.target.value)}
+          width="100%"
+        />
+        <h3>{result}</h3>
+        <Grid container justifyContent="space-between" spacing={2}>
+          <Grid item size={3}>
+            <button onClick={handleExp}>7</button>
+          </Grid>
+          <Grid item size={3}>
+            <button onClick={handleExp}>8</button>
+          </Grid>
+          <Grid item size={3}>
+            <button onClick={handleExp}>9</button>
+          </Grid>
+          <Grid item size={3}>
+            <button onClick={handleExp}>+</button>
+          </Grid>
+          <Grid item size={3}>
+            <button onClick={handleExp}>4</button>
+          </Grid>
+          <Grid item size={3}>
+            <button onClick={handleExp}>5</button>
+          </Grid>
+          <Grid item size={3}>
+            <button onClick={handleExp}>6</button>
+          </Grid>
+          <Grid item size={3}>
+            <button onClick={handleExp}>-</button>
+          </Grid>
+          <Grid item size={3}>
+            <button onClick={handleExp}>1</button>
+          </Grid>
+          <Grid item size={3}>
+            <button onClick={handleExp}>2</button>
+          </Grid>
+          <Grid item size={3}>
+            <button onClick={handleExp}>3</button>
+          </Grid>
+          <Grid item size={3}>
+            <button onClick={handleExp}>*</button>
+          </Grid>
+          <Grid item size={3}>
+            <button onClick={() => setExp("")}>C</button>
+          </Grid>
+          <Grid item size={3}>
+            <button onClick={handleExp}>0</button>
+          </Grid>
+          <Grid item size={3}>
+            <button onClick={showResult}>=</button>
+          </Grid>
+          <Grid item size={3}>
+            <button onClick={handleExp}>/</button>
+          </Grid>
+        </Grid>
       </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
